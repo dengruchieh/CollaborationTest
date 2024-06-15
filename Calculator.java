7e1b70683b3b968ab00ed01fc9e15897a8333c37
dengruchieh <deng820906@gmail.com>
2024-06-15 16:33:22 +0800

    feat: add comment

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,10 @@
 public class Calculator {
+    //add
     public int add(int a, int b) {
         return a + b;
     }
 
+    //subtract
     public int subtract(int a, int b) {
         return a - b;
     }
